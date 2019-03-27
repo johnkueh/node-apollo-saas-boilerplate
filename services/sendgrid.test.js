f87891b82ca126cdbb4c53153351d58c1a1d4dcf
John Kueh <yoongfook@gmail.com>
2019-03-27 22:22:39 +1100

    Added sendgrid unit test

--- services/sendgrid.test.js
+++ services/sendgrid.test.js
@@ -1,9 +1,14 @@
-import { msg } from './sendgrid';
+import sgMail from '@sendgrid/mail';
+import msg from './sendgrid';
+
+jest.mock('@sendgrid/mail');
 
 beforeEach(() => {
-  process.env = {
-    SENDGRID_API_KEY: 'MOCK-KEY'
-  };
+  process.env.SENDGRID_API_KEY = 'MOCK-KEY';
 });
 
-test('msg calls .send with the message on a SendGrid mail service', () => {});
+test('msg calls .send with the message on a SendGrid mail service', () => {
+  msg('Hello there');
+  expect(sgMail.setApiKey).toHaveBeenCalledWith('MOCK-KEY');
+  expect(sgMail.send).toHaveBeenCalledWith('Hello there');
+});
