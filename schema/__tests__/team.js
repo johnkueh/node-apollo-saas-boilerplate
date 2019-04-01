fb7e4194ef12720e7d7a4ebaf72a412b58b56b05
John Kueh <yoongfook@gmail.com>
2019-04-01 22:42:13 +1100

    Added unit tests for graphql schema definitions

---
+++ schema/__tests__/team.js
@@ -0,0 +1,71 @@
+import { mockServer } from 'graphql-tools';
+import schema from '../index';
+
+const mocks = {
+  Boolean: () => false,
+  ID: () => '1',
+  Int: () => 1,
+  Float: () => 12.34,
+  String: () => 'String',
+  DateTime: () => new Date(2018, 8, 8)
+};
+
+const server = mockServer(schema, mocks, false);
+
+describe('Team schema', () => {
+  it('has valid type definitions', () => {
+    expect(() => server.query(`{ __schema { types { name } } }`)).not.toThrow();
+  });
+
+  it(`mutation { createTeam } should return expected value`, () => {
+    const payload = `
+      mutation CreateTeam($name: String!) {
+        createTeam(name: $name) {
+          id
+          name
+        }
+      }
+    `;
+
+    const variables = {
+      name: 'Awesome team'
+    };
+
+    const expected = {
+      data: {
+        createTeam: {
+          id: mocks.String(),
+          name: mocks.String()
+        }
+      }
+    };
+
+    expect(server.query(payload, variables)).resolves.toEqual(expected);
+  });
+
+  it(`mutation { updateTeam } should return expected value`, () => {
+    const payload = `
+      mutation UpdateTeam($name: String!) {
+        updateTeam(name: $name) {
+          id
+          name
+        }
+      }
+    `;
+
+    const variables = {
+      name: 'Awesome team 2'
+    };
+
+    const expected = {
+      data: {
+        updateTeam: {
+          id: mocks.String(),
+          name: mocks.String()
+        }
+      }
+    };
+
+    expect(server.query(payload, variables)).resolves.toEqual(expected);
+  });
+});
